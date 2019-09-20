3436b0d5196681f3e2486e16ff1962326638e409
Vaibhav Singh <think.vaibhavsingh@gmail.com>
2019-09-20 13:33:53 +0200

    Added connection to mongoose

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//mongoose.connect('mongodb+srv://SpoiL3r:qwerty123@cluster0-0blze.mongodb.net/test?retryWrites=true&w=majority',{
+ //   useMongoClient = true
+//});
+
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Routes handling requests
 app.use('/products', productRoutes); 
